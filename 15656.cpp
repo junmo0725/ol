c08d358bf9c6b07fc1226e4d72c7306209b0b5ab
junmo0725 <sb23040923@cberi.go.kr>
2025-07-20 05:20:05 +0000

    15656

---
+++ 15656.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> ans;
+vector<int>l;
+int n, m, arr[9];
+
+void recu(int k) {
+    if (ans.size() >= m) {
+        for (auto c : ans)
+            cout << c << " ";
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < n; i++) 
+    {
+        ans.push_back(l[i]);
+        recu(i+1);
+        l[i]=ans.back();
+        ans.pop_back();
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++){
+        int temp;
+        cin>>temp;
+        l.push_back(temp);
+    }
+    sort(l.begin(), l.end());
+    recu(0);
+    return 0;
+}
